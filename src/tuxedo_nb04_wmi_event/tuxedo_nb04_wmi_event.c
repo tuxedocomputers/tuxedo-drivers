4c53ef0dfa0f7324e1d3d95fa899ad8ceac0d689
Christoffer Sandberg <cs@tuxedo.de>
2023-12-13 12:38:24 +0100

    nb04: Initial WMI event driver
    - Input device driver
    - Map touchpad toggle event

---
+++ src/tuxedo_nb04_wmi_event/tuxedo_nb04_wmi_event.c
@@ -0,0 +1,187 @@
+/*!
+ * Copyright (c) 2023 TUXEDO Computers GmbH <tux@tuxedocomputers.com>
+ *
+ * This file is part of tuxedo-keyboard.
+ *
+ * tuxedo-keyboard is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+#include <linux/acpi.h>
+#include <linux/module.h>
+#include <linux/wmi.h>
+#include <linux/keyboard.h>
+#include <linux/input.h>
+#include <linux/input/sparse-keymap.h>
+#include <linux/version.h>
+#include <linux/delay.h>
+
+#define NB04_WMI_EVENT_GUID	"96A786FA-690C-48FB-9EB3-FA9BC3D92300"
+#define NB04_WMI_BS_GUID	"1F174999-3A4E-4311-900D-7BE7166D5055"
+#define NB04_WMI_AB_GUID	"80C9BAA6-AC48-4538-9234-9F81A55E7C85"
+
+#define NB04_WMI_EVENT_MODE_BATTERY			0x01
+#define NB04_WMI_EVENT_MODE_HUMAN			0x02
+#define NB04_WMI_EVENT_MODE_BEAST			0x03
+#define NB04_WMI_EVENT_FULL_FAN				0x04
+#define NB04_WMI_EVENT_NEXT_POWER_MODE			0x05
+#define NB04_WMI_EVENT_TOUCHPAD_TOGGLE			0x06
+#define NB04_WMI_EVENT_MIC_MUTE				0x07
+#define NB04_WMI_EVENT_KBD_BRT_UP			0x08
+#define NB04_WMI_EVENT_KBD_BRT_DOWN			0x09
+#define NB04_WMI_EVENT_KBD_EFFECT_ALWAYS		0x0A
+#define NB04_WMI_EVENT_KBD_EFFECT_BREATHING		0x0B
+#define NB04_WMI_EVENT_KBD_EFFECT_WAVE			0x0C
+#define NB04_WMI_EVENT_KBD_EFFECT_TWINKLE		0x0D
+#define NB04_WMI_EVENT_KBD_EFFECT_COLOR_CYCLE		0x0E
+#define NB04_WMI_EVENT_KBD_EFFECT_REACTIVE		0x0F
+#define NB04_WMI_EVENT_KBD_EFFECT_RIPPLE		0x10
+#define NB04_WMI_EVENT_KBD_EFFECT_SPIRAL_RAINBOW	0x11
+#define NB04_WMI_EVENT_KBD_EFFECT_RAINBOW_RIPPLE	0x12
+
+struct driver_data_t {
+	struct input_dev *input_dev;
+};
+
+static struct key_entry driver_keymap[] = {
+	{ KE_KEY,	NB04_WMI_EVENT_TOUCHPAD_TOGGLE,	{ KEY_F21 } },
+	{ KE_END,	0 }
+};
+
+static int input_device_init(struct input_dev **input_dev_pp, const struct key_entry key_map[])
+{
+	struct input_dev *input_dev;
+	int err;
+
+	input_dev = input_allocate_device();
+	if (unlikely(!input_dev)) {
+		pr_err("Error allocating input device\n");
+		return -ENOMEM;
+	}
+
+	input_dev->name = "TUXEDO Keyboard Events";
+	input_dev->phys = "tuxedo-keyboard" "/input0";
+	input_dev->id.bustype = BUS_HOST;
+	input_dev->dev.parent = NULL;
+
+	if (key_map != NULL) {
+		err = sparse_keymap_setup(input_dev, key_map, NULL);
+		if (err) {
+			pr_err("Failed to setup sparse keymap\n");
+			goto err_free_input_device;
+		}
+	}
+
+	err = input_register_device(input_dev);
+	if (unlikely(err)) {
+		pr_err("Error registering input device\n");
+		goto err_free_input_device;
+	}
+
+	*input_dev_pp = input_dev;
+
+	return 0;
+
+err_free_input_device:
+	input_free_device(input_dev);
+
+	return err;
+}
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 3, 0)
+static int tuxedo_nb04_wmi_event_probe(struct wmi_device *wdev)
+#else
+static int tuxedo_nb04_wmi_event_probe(struct wmi_device *wdev, const void *dummy_context)
+#endif
+{
+	struct driver_data_t *driver_data;
+	int err;
+
+	pr_debug("driver probe\n");
+
+	driver_data = devm_kzalloc(&wdev->dev, sizeof(*driver_data), GFP_KERNEL);
+	if (!driver_data)
+		return -ENOMEM;
+
+	dev_set_drvdata(&wdev->dev, driver_data);
+
+	err = input_device_init(&driver_data->input_dev, driver_keymap);
+	if (err)
+		return err;
+
+	return 0;
+}
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 13, 0)
+static int tuxedo_nb04_wmi_event_remove(struct wmi_device *wdev)
+#else
+static void tuxedo_nb04_wmi_event_remove(struct wmi_device *wdev)
+#endif
+{
+	pr_debug("driver remove\n");
+	struct driver_data_t *driver_data = dev_get_drvdata(&wdev->dev);
+	input_unregister_device(driver_data->input_dev);
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 13, 0)
+	return 0;
+#endif
+}
+
+static void tuxedo_nb04_wmi_event_notify(struct wmi_device *wdev, union acpi_object *obj)
+{
+	u8 function_number;
+	u8 event_code;
+	struct driver_data_t *driver_data = dev_get_drvdata(&wdev->dev);
+
+	if (obj &&
+	    obj->type == ACPI_TYPE_BUFFER &&
+	    obj->buffer.length >= 2)
+	{
+		function_number = obj->buffer.pointer[0];
+		event_code = obj->buffer.pointer[1];
+		pr_debug("event value: %d (%0#4x)\n",
+			 event_code, event_code);
+		sparse_keymap_report_event(driver_data->input_dev,
+						 event_code,
+						 1,
+						 true);
+	} else {
+		pr_debug("expected buffer not found\n");
+	}
+}
+
+static const struct wmi_device_id tuxedo_nb04_wmi_event_device_ids[] = {
+	{ .guid_string = NB04_WMI_EVENT_GUID },
+	{ }
+};
+
+static struct wmi_driver tuxedo_nb04_wmi_event_driver = {
+	.driver = {
+		.name = "tuxedo_nb04_wmi_event",
+		.owner = THIS_MODULE
+	},
+	.id_table = tuxedo_nb04_wmi_event_device_ids,
+	.probe = tuxedo_nb04_wmi_event_probe,
+	.remove = tuxedo_nb04_wmi_event_remove,
+	.notify = tuxedo_nb04_wmi_event_notify,
+};
+
+module_wmi_driver(tuxedo_nb04_wmi_event_driver);
+
+MODULE_AUTHOR("TUXEDO Computers GmbH <tux@tuxedocomputers.com>");
+MODULE_DESCRIPTION("Driver for NB04 WMI events");
+MODULE_VERSION("0.0.1");
+MODULE_LICENSE("GPL");
+
+MODULE_DEVICE_TABLE(wmi, tuxedo_nb04_wmi_event_device_ids);
+MODULE_ALIAS("wmi:" NB04_WMI_EVENT_GUID);
