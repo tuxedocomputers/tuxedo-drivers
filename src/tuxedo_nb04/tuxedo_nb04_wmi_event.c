014f77e57887ac5cf7a76baaf11f54ec6543af42
Christoffer Sandberg <cs@tuxedo.de>
2023-12-18 12:43:44 +0100

    nb04: Add kbd backlight brightness up/down mapping

--- src/tuxedo_nb04/tuxedo_nb04_wmi_event.c
+++ src/tuxedo_nb04/tuxedo_nb04_wmi_event.c
@@ -53,6 +53,8 @@ struct driver_data_t {
 
 static struct key_entry driver_keymap[] = {
 	{ KE_KEY,	NB04_WMI_EVENT_TOUCHPAD_TOGGLE,	{ KEY_F21 } },
+	{ KE_KEY,	NB04_WMI_EVENT_KBD_BRT_DOWN,	{ KEY_KBDILLUMDOWN } },
+	{ KE_KEY,	NB04_WMI_EVENT_KBD_BRT_UP,	{ KEY_KBDILLUMUP } },
 	{ KE_END,	0 }
 };
 
