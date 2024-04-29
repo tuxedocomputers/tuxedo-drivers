4b79429096db9b996c5efd63892a0b04846a7053
Christoffer Sandberg <cs@tuxedo.de>
2024-04-29 15:40:54 +0200

    tuxi: Implement fan acpi control method interfacing

--- src/tuxedo_tuxi/tuxi_acpi.c
+++ src/tuxedo_tuxi/tuxi_acpi.c
@@ -32,33 +32,163 @@ struct tuxi_acpi_driver_data_t {
 
 static struct tuxi_acpi_driver_data_t *tuxi_driver_data = NULL;
 
+static
+int evaluate_intparams(acpi_handle handle,
+		       acpi_string pathname,
+		       unsigned long long *int_params,
+		       u32 param_count,
+		       unsigned long long *retval);
+
+static
+int evaluate_intparams(acpi_handle handle,
+		       acpi_string pathname,
+		       unsigned long long *int_params,
+		       u32 param_count,
+		       unsigned long long *retval)
+{
+	struct acpi_object_list input;
+	union acpi_object *params;
+	unsigned long long result;
+	acpi_status status;
+	int i;
+	u32 param_buffer_size = sizeof(union acpi_object) * param_count;
+
+	if (param_buffer_size > 0)
+		params = kzalloc(param_buffer_size, GFP_KERNEL);
+
+	for (i = 0; i < param_count; ++i) {
+		params[i].type = ACPI_TYPE_INTEGER;
+		params[i].integer.value = int_params[i];
+	}
+
+	input.count = param_count;
+	input.pointer = params;
+
+	if (param_buffer_size > 0) {
+		status = acpi_evaluate_integer(handle, pathname, &input, &result);
+		kfree(params);
+	} else {
+		status = acpi_evaluate_integer(handle, pathname, NULL, &result);
+	}
+
+	if (ACPI_FAILURE(status))
+		return -EIO;
+
+	if (retval)
+		*retval = result;
+
+	return 0;
+}
+
 int tuxi_set_fan_speed(u8 fan_index, u8 fan_speed)
 {
+	long long int retval;
+	long long int args[] = { fan_index, fan_speed };
+	int err;
+	err = evaluate_intparams(tuxi_driver_data->tfan_handle,
+				 "SSPD",
+				 args, ARRAY_SIZE(args),
+				 &retval);
+	if (err)
+		return err;
+
+	if (retval)
+		return -EINVAL;
+
 	return 0;
 }
 
 int tuxi_get_fan_speed(u8 fan_index, u8 *fan_speed)
 {
+	long long int retval;
+	long long int args[] = { fan_index };
+	int err;
+	err = evaluate_intparams(tuxi_driver_data->tfan_handle,
+				 "GSPD",
+				 args, ARRAY_SIZE(args),
+				 &retval);
+	if (err)
+		return err;
+
+	if (retval < 0)
+		return -EINVAL;
+
+	*fan_speed = (u8) retval;
+
 	return 0;
 }
 
 int tuxi_get_nr_fans(u8 *nr_fans)
 {
+	long long int retval;
+	int err;
+	err = evaluate_intparams(tuxi_driver_data->tfan_handle,
+				 "GCNT",
+				 NULL, 0,
+				 &retval);
+	if (err)
+		return err;
+
+	*nr_fans = (u8) retval;
+
 	return 0;
 }
 
+// TODO: mode enum signature
 int tuxi_set_fan_mode(u8 mode)
 {
+	long long int retval;
+	long long int args[] = { mode };
+	int err;
+	err = evaluate_intparams(tuxi_driver_data->tfan_handle,
+				 "SMOD",
+				 args, ARRAY_SIZE(args),
+				 &retval);
+	if (err)
+		return err;
+
+	if (retval < 0)
+		return -EINVAL;
+
 	return 0;
 }
 
 int tuxi_get_fan_mode(u8 *mode)
 {
+	long long int retval;
+	int err;
+	err = evaluate_intparams(tuxi_driver_data->tfan_handle,
+				 "GMOD",
+				 NULL, 0,
+				 &retval);
+	if (err)
+		return err;
+
+	if (retval < 0)
+		return -EINVAL;
+
+	*mode = (u8) retval;
+
 	return 0;
 }
 
 int tuxi_get_fan_type(u8 fan_index, enum tuxi_fan_type *type)
 {
+	long long int retval;
+	long long int args[] = { fan_index };
+	int err;
+	err = evaluate_intparams(tuxi_driver_data->tfan_handle,
+				 "GTYP",
+				 args, ARRAY_SIZE(args),
+				 &retval);
+	if (err)
+		return err;
+
+	if (retval < 0)
+		return -EINVAL;
+
+	*type = (u8) retval;
+
 	return 0;
 }
 
