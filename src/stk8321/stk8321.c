0b47c30a213e19b6c02724278e35b4259bb376a2
Werner Sembach <wse@tuxedocomputers.com>
2024-11-08 18:38:01 +0100

    Fix copy paste error

--- src/stk8321/stk8321.c
+++ src/stk8321/stk8321.c
@@ -508,4 +508,4 @@ module_i2c_driver(stk8321_driver);
 
 MODULE_AUTHOR("TUXEDO Computers GmbH <tux@tuxedocomputers.com>");
 MODULE_DESCRIPTION("STK8321 3-Axis Accelerometer driver");
-MODULE_LICENSE("GPL v2");
+MODULE_LICENSE("GPL");
