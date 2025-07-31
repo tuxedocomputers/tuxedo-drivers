5497c138b56e25013d78a753ba6bf08166085069
Tim Guttzeit <t.guttzeit@tuxedocomputers.com>
2025-07-31 08:58:27 +0200

    ite_8291: Fixed Stellaris Gen7 AMD Keyboard color
    Added it to the colour fix of intel variation

--- src/ite_8291/ite_8291.c
+++ src/ite_8291/ite_8291.c
@@ -215,8 +215,8 @@ static void color_scaling(struct hid_device *hdev, u8 *red, u8 *green, u8 *blue,
 			*red = (148 * *red) / 255;
 			*blue = (137 * *blue) / 255;
 		}
-	} else if (dmi_match(DMI_PRODUCT_SKU, "STELLARIS16I07")
-			&& hdev->product == 0x600b) {
+	} else if ((dmi_match(DMI_PRODUCT_SKU, "STELLARIS16I07") || dmi_match(DMI_PRODUCT_SKU, "STELLARIS16A07"))
+		   && hdev->product == 0x600b) {
 		// all keys: reduce pink
 		*red = (170 * *red) / 255;
 		*blue = (125 * *blue) / 255;
