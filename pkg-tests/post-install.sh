3f6b289f75ba5013888eeb7dc1a1d02023184940
Aaron Erhardt <aer@tuxedocomputers.com>
2026-04-08 15:01:39 +0200

    fix: don't test dkms on arch

--- pkg-tests/post-install.sh
+++ pkg-tests/post-install.sh
@@ -2,6 +2,11 @@
 
 set -eu
 
+if [ "$1" = "alpm" ];
+    # Don't check on Arch (no dkms install is done according to guidelines)
+    exit 0
+fi
+
 status="$(dkms status)"
 
 echo $status | grep "tuxedo-drivers" # Make sure dkms module is installed
